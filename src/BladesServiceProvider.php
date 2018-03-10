0ac95ff54134158a17c40b7bf0e5ca01c541790f
Howar31 <howar31@gmail.com>
2018-03-10 19:31:04 +0800

    Add webpack for asset compile. Add demo image files.

--- src/BladesServiceProvider.php
+++ src/BladesServiceProvider.php
@@ -18,6 +18,8 @@ class BladesServiceProvider extends ServiceProvider
 			__DIR__.'/views' => resource_path('views'),
 			__DIR__.'/controllers' => app_path('Http/Controllers'),
 			__DIR__.'/routes.php' => base_path('routes/web.php'),
+			__DIR__.'/images' => public_path('images'),
+			__DIR__.'/webpack.mix.js' => base_path('webpack.mix.js'),
 		], 'admin');
 	}
 
