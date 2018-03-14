b3ac8372ebcc3e410e87d80be4840b76f69fadc1
Howar31 <howar31@gmail.com>
2018-03-14 16:59:56 +0800

    Fix navbar layout while collapse in smallscreen mode.

--- src/views/admin/layout/topbar.blade.php
+++ src/views/admin/layout/topbar.blade.php
@@ -5,11 +5,8 @@
 			<i class="fas fa-bars"></i>
 		</button>
 	</div>
-	<nav class="navbar navbar-expand-lg navbar-light" role="navigation">
-		<a class="navbar-brand" href="{{ url('/') }}">Navbar</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
+	<nav class="navbar navbar-expand-md navbar-light" role="navigation">
+		<a class="navbar-brand" href="{{ url('/') }}">Project Name</a>
 
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav mr-auto">
@@ -34,23 +31,17 @@
 					<a class="nav-link disabled" href="#">Disabled</a>
 				</li>
 			</ul>
-			<form class="form-inline my-2 my-lg-0">
-				<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-			</form>
-			<ul class="navbar-nav">
-				<li class="nav-item dropdown">
-					<a class="nav-link user-menu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						<i class="fas fa-user-circle"></i>
-					</a>
-					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-						<a class="dropdown-item" href="#">Profile</a>
-						<a class="dropdown-item" href="#">Settings</a>
-						<div class="dropdown-divider"></div>
-						<a class="dropdown-item" href="#">Logout</a>
-					</div>
-				</li>
-			</ul>
+		</div>
+		<div class="dropdown user-menu">
+			<a href="javascript:void(0)" role="button" id="userDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				<i class="fas fa-user-circle"></i> Howar31
+			</a>
+			<div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+				<a class="dropdown-item" href="#">Profile</a>
+				<a class="dropdown-item" href="#">Settings</a>
+				<div class="dropdown-divider"></div>
+				<a class="dropdown-item" href="#">Logout</a>
+			</div>
 		</div>
 	</nav>
 </div>
