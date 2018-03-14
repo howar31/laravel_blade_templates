0c9c50ffbad429f11e54b246b780fc672f42dc7a
Howar31 <howar31@gmail.com>
2018-03-14 17:46:37 +0800

    Add Faker for data testing.

--- src/views/admin/dashboard.blade.php
+++ src/views/admin/dashboard.blade.php
@@ -4,24 +4,5 @@
 ])
 
 @section('content')
-	<h1 data-toggle="tooltip" data-placement="left" title="test left">This is a TEST00</h1>
-	<h1 data-toggle="tooltip" data-placement="right" title="test right">This is a TEST01</h1>
-	<h1 data-toggle="tooltip" data-placement="top" title="test top">This is a TEST02</h1>
-	<h1 data-toggle="tooltip" data-placement="bottom" title="test bottom">This is a TEST03</h1>
-	<h1 data-toggle="tooltip" data-placement="auto" title="test auto">This is a TEST04</h1>
-	<h1>This is a TEST05</h1>
-	<h1>This is a TEST06</h1>
-	<h1>This is a TEST07</h1>
-	<h1>This is a TEST08</h1>
-	<h1>This is a TEST09</h1>
-	<h1>This is a TEST10</h1>
-	<h1>This is a TEST11</h1>
-	<h1>This is a TEST12</h1>
-	<h1>This is a TEST13</h1>
-	<h1>This is a TEST14</h1>
-	<h1>This is a TEST15</h1>
-	<h1>This is a TEST16</h1>
-	<h1>This is a TEST17</h1>
-	<h1>This is a TEST18</h1>
-	<h1>This is a TEST19</h1>
+	<h1>Dashboard</h1>
 @endsection
--- src/views/admin/overview.blade.php
+++ src/views/admin/overview.blade.php
@@ -3,6 +3,20 @@
 	'title' => 'Overview',
 ])
 
+@php
+	$faker = Faker\Factory::create();
+@endphp
+
 @section('content')
-	<h1>This is OVERVIEW</h1>
+	<h1>Overview</h1>
+	<p>This is a template project for Laravel Blade.</p>
+
+	<hr>
+
+	<h1>Test</h1>
+	<p>Data below are using for testing.</p>
+	@for ($i = 0; $i < 20; $i++)
+		<p>{{ $faker->realText($maxNbChars = $faker->numberBetween(200, 2000)) }}</p>
+	@endfor
+	<hr>
 @endsection
