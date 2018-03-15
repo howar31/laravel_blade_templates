db5d19a273273d51904de8c8fc4f427c84f58fa7
Howar31 <howar31@gmail.com>
2018-03-15 15:14:59 +0800

    Add Badge for Sidebar.

--- src/config/admin/sidebar.php
+++ src/config/admin/sidebar.php
@@ -4,16 +4,19 @@
 //	[	// Sidebar menu item
 //		"text" => "",	// Text to display for sidebar menu item
 //		"icon" => "",	// FontAwesome icon class
+//		"badge" => "",	// Badge text
 //		"url" => "",	// [Exclusive for Item]  URL for the sidebar menu item
 //		"current_page" => ""	// [Exclusive for Item] Page ID used for active highlight (set in section)
 //	],
 //	[	// Sidebar menu group
 //		"text" => "",	// Text to display for sidebar menu group title
 //		"icon" => "",	// FontAwesome icon class
+//		"badge" => "",	// Badge text
 //		"children" => [	// Sidebar menu group wrap
 //			[	// Sidebar sub-menu item
 //				"text" => "",	// Text to display for sidebar menu item
 //				"icon" => "",	// FontAwesome icon class
+//				"badge" => "",	// Badge text
 //				"url" => "",	// [Exclusive for Item]  URL for the sidebar menu item
 //				"current_page" => ""	// [Exclusive for Item] Page ID used for active highlight (set in section)
 //			]
@@ -24,70 +27,82 @@ return [
 	[
 		"text" => "Overview",
 		"icon" => "fas fa-home",
+		"badge" => "HOME",
 		"url" => "admin/overview",
 		"current_page" => "overview"
 	],
 	[
 		"text" => "Dashboard",
 		"icon" => "fas fa-clipboard-list",
+		"badge" => "",
 		"url" => "admin/dashboard",
 		"current_page" => "dashboard"
 	],
 	[
 		"text" => "UI Components",
 		"icon" => "fas fa-magic",
+		"badge" => "",
 		"children" => [
 			[
 				"text" => "Alerts",
 				"icon" => "fas fa-exclamation",
+				"badge" => "",
 				"url" => "admin/ui/alert",
 				"current_page" => "alert"
 			],
 			[
 				"text" => "Buttons",
 				"icon" => "fas fa-keyboard",
+				"badge" => "",
 				"url" => "admin/ui/button",
 				"current_page" => "button"
 			],
 			[
 				"text" => "Grids",
 				"icon" => "fas fa-th-large",
+				"badge" => "",
 				"url" => "admin/ui/grid",
 				"current_page" => "grid"
 			],
 			[
 				"text" => "Modals",
 				"icon" => "fas fa-window-restore",
+				"badge" => "",
 				"url" => "admin/ui/modal",
 				"current_page" => "modal"
 			],
 			[
 				"text" => "Tables",
 				"icon" => "fas fa-table",
+				"badge" => "",
 				"url" => "admin/ui/table",
 				"current_page" => "table"
 			],
 			[
 				"text" => "Toasts",
 				"icon" => "fas fa-bell",
+				"badge" => "",
 				"url" => "admin/ui/toast",
 				"current_page" => "toast"
 			],
 			[
 				"text" => "Panels",
 				"icon" => "fas fa-window-maximize",
+				"badge" => "",
 				"url" => "admin/ui/panel",
 				"current_page" => "panel"
 			],
 			[
 				"text" => "Progress Bar",
 				"icon" => "fas fa-spinner",
+				"badge" => "",
 				"url" => "admin/ui/progressbar",
 				"current_page" => "progressbar"
 			],
 			[
 				"text" => "Typography",
 				"icon" => "fas fa-at",
+				"badge" => "",
 				"url" => "admin/ui/typography",
 				"current_page" => "typography"
 			]
@@ -96,10 +111,12 @@ return [
 	[
 		"text" => "Forms",
 		"icon" => "fab fa-wpforms",
+		"badge" => "",
 		"children" => [
 			[
 				"text" => "Login",
 				"icon" => "fas fa-sign-in-alt",
+				"badge" => "",
 				"url" => "admin/form/login",
 				"current_page" => "login"
 			]
@@ -107,11 +124,13 @@ return [
 	],
 	[
 		"text" => "Charts",
-		"icon" => "",
+		"icon" => "fas fa-chart-line",
+		"badge" => "",
 		"children" => [
 			[
 				"text" => "General",
 				"icon" => "fas fa-chart-pie",
+				"badge" => "",
 				"url" => "admin/chart/general",
 				"current_page" => "general"
 			]
@@ -120,16 +139,19 @@ return [
 	[
 		"text" => "About",
 		"icon" => "fas fa-pencil-alt",
+		"badge" => "",
 		"children" => [
 			[
 				"text" => "Laravel Blade Templates",
 				"icon" => "fab fa-laravel",
+				"badge" => "",
 				"url" => "admin/about/template",
 				"current_page" => "template"
 			],
 			[
 				"text" => "Author",
 				"icon" => "fas fa-user",
+				"badge" => "",
 				"url" => "admin/about/author",
 				"current_page" => "author"
 			]
--- src/views/admin/layout/sidebar.blade.php
+++ src/views/admin/layout/sidebar.blade.php
@@ -3,22 +3,25 @@
 		<ul>
 			@foreach ($sidebar as $item)
 			@if (! isset($item['children']))
-			<li class="sidebar-item{{ ($item['current_page'] == $current_page) ? ' active' : '' }}">
-				<a href="{{ url($item['url']) }}">
-					<i class="{{ $item['icon'] }}"></i> {{ $item['text'] }}
+			<li class="sidebar-item{{ (array_get($item, 'current_page') == $current_page) ? ' active' : '' }}">
+				<a href="{{ url(array_get($item, 'url')) }}">
+					<span class="pull-right"><span class="badge">{{ array_get($item, 'badge') }}</span></span>
+					<i class="{{ array_get($item, 'icon') }}"></i> {{ array_get($item, 'text') }}
 				</a>
 			</li>
 			@else
 			<li class="sidebar-group">
 				<a class="sidebar-group-title" href="javascript:void(0);">
 					<span class="group-arrow pull-right"><i class="fas fa-angle-down"></i></span>
-					<i class="$item['icon']"></i> {{ $item['text'] }}
+					<span class="pull-right"><span class="badge">{{ array_get($item, 'badge') }}</span></span>
+					<i class="{{ array_get($item, 'icon') }}"></i> {{ array_get($item, 'text') }}
 				</a>
 				<ul class="sidebar-group-content">
 					@foreach ($item['children'] as $subitem)
-					<li class="sidebar-item{{ ($subitem['current_page'] == $current_page) ? ' active' : '' }}"">
-						<a href="{{ url($subitem['url']) }}">
-							<i class="{{ $subitem['icon'] }}"></i> {{ $subitem['text'] }}
+					<li class="sidebar-item{{ (array_get($subitem, 'current_page') == $current_page) ? ' active' : '' }}"">
+						<a href="{{ url(array_get($subitem, 'url')) }}">
+							<span class="pull-right"><span class="badge">{{ array_get($subitem, 'badge') }}</span></span>
+							<i class="{{ array_get($subitem, 'icon') }}"></i> {{ array_get($subitem, 'text') }}
 						</a>
 					</li>
 					@endforeach
