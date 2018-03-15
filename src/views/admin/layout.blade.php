c5d099d5b42a91c41b9cae56710fffeef8eb4d85
Howar31 <howar31@gmail.com>
2018-03-15 16:38:08 +0800

    Change layout to much more general module.

--- src/views/admin/layout.blade.php
+++ src/views/admin/layout.blade.php
@@ -1,41 +1,14 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-		<meta name="mobile-web-app-capable" content="yes">
-		<meta name="apple-mobile-web-app-capable" content="yes">
-		<meta name="description" content="Admin Template by Howar31">
-		<meta name="author" content="Howar31">
+@extends('layout')
 
-		<title>Admin Template{{ empty($title) ? '' : ' - ' . $title }}</title>
+@section('head_append')
+	<link rel="stylesheet" href="{{ URL::asset('css/admin.css') }}">
+@endsection
 
-		<!-- Bootstrap 4 -->
-		<link rel="stylesheet" href="{{ URL::asset('vendor/bootstrap/bootstrap.min.css') }}">
+@section('body_prepend')
+	@include('admin.layout.topbar')
+	@include('admin.layout.sidebar', ['sidebar' => config('admin.sidebar')])
+@endsection
 
-		<link rel="stylesheet" href="{{ URL::asset('css/admin.css') }}">
-	</head>
-	<body>
-		@include('admin.layout.topbar')
-		@include('admin.layout.sidebar', ['sidebar' => config('admin.sidebar')])
-
-		<div id="content">
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-md-12">
-						@yield('content')
-					</div>
-				</div>
-			</div>
-		</div>
-		{{-- @include('admin.layout.footer') --}}
-
-		<!-- FontAwesome -->
-		<script defer src="{{ URL::asset('/vendor/fontawesome/fontawesome-all.min.js') }}"></script>
-		<!-- Bootstrap 4 -->
-		<script src="{{ URL::asset('/vendor/jquery/jquery.min.js') }}"></script>
-		<script src="{{ URL::asset('/vendor/bootstrap/bootstrap.bundle.min.js') }}"></script>
-
-		<script src="{{ URL::asset('js/admin.js') }}"></script>
-	</body>
-</html>
+@section('body_append')
+	<script src="{{ URL::asset('js/admin.js') }}"></script>
+@endsection
